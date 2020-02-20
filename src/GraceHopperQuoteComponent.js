2bd3eeccc0f1e43c0bb8aadc166e8b56366aa274
sylwiavargas <sylwia.vargas@gmail.com>
2020-02-20 15:52:22 -0500

    error cleanup

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class GraceHopperQuoteComponent extends Component {
 	render() {
 		return (
-			<div className="bar" id="einstein">
+			<div className="bar" id="hopper">
 				<i>
 					<p>
 						&quot;The most dangerous phrase in the language is, ‘We’ve always done it this way.’&quot;
